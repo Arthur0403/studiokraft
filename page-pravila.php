28e05c0ff705d68befb6d7dd7eeccb10e72df8e5
powerteks <jit.prophet@gmail.com>
2020-05-24 20:43:36 +0300

    fix style for title

--- page-pravila.php
+++ page-pravila.php
@@ -11,7 +11,7 @@ get_header();
     <section class="py-0 pb-0 container">
         <div class="row">
             <div class="page-content col-lg-12 py-4 px-5 mx-auto pages">
-                <h1><?php the_title(); ?></h1>
+                <h1 class="ta-center"><?php the_title(); ?></h1>
             </div>
         </div>
     </section>
