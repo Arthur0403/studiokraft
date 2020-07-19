d2a8466ccb0d646005a4c00a9d90edd9571ac70e
Arthur <alobanov1989@yandex.ru>
2020-07-19 13:52:19 +0300

    fix slider in hall page

--- page-halls.php
+++ page-halls.php
@@ -9,7 +9,7 @@ get_header();
 ?>
 
 <div class="container-fluid p-0">
-  <div class="container">
+  <div class="container container-halls">
     <?php if( is_page( 'grey' ) ) {
             echo do_shortcode('[slick-slider category="5" fade="true" autoplay_interval="5000" image_size="original" design="design-2" lazyload="ondemand" arrow="false" image_fit="true"]');
           }
